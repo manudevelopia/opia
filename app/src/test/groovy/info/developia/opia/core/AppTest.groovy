05e6e7db17efe082f79d650970271fdca6657ee3
manudevelopia <manu@developia.info>
2022-03-20 23:34:39 +0100

    kickoff repository

---
+++ app/src/test/groovy/info/developia/opia/core/AppTest.groovy
@@ -0,0 +1,16 @@
+package info.developia.opia.core
+
+import spock.lang.Specification
+
+class AppTest extends Specification {
+    def "application has a greeting"() {
+        setup:
+        def app = new App()
+
+        when:
+        def result = app.greeting
+
+        then:
+        result != null
+    }
+}
